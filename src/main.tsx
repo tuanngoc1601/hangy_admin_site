1f58e5d4c72fc9bfb7273c585f71dd57c3386a9e
tuanngoc1601 <tuanngoc12340@gmail.com>
2024-11-09 19:51:47 +0700

    fix: svg icon components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { SidebarProvider } from "./context/SidebarContext.tsx";
+import ThemedSuspense from "./components/ThemedSuspense.tsx";
+import windmillTheme from "./windmillTheme.ts";
+import { Windmill } from "@windmill/react-ui";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <SidebarProvider>
+      <Suspense fallback={<ThemedSuspense />}>
+        <Windmill usePreferences theme={windmillTheme}>
+          <App />
+        </Windmill>
+      </Suspense>
+    </SidebarProvider>
+    ,
+  </StrictMode>
+);
